0aa44cb2f087bb7dcb541324d94d464f5beb643e
Ariel Andrade <lima.ariel97@gmail.com>
2021-12-24 20:08:43 -0300

    allowing window to be resizable

--- main.lua
+++ main.lua
@@ -15,6 +15,10 @@ PADDLE_SPEED = 200
 MAX_SCORE = 10
 
 
+function love.resize(w, h)
+    push:resize(w, h)    
+end
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     love.window.setTitle('Pong')
@@ -33,7 +37,7 @@ function love.load()
     
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT , {
         fullscreen = false,
-        resizable = false,
+        resizable = true,
         vsync = true
     })    
 
@@ -51,6 +55,7 @@ end
 function love.draw() 
     push:apply('start')
     love.graphics.setFont(smallFont)
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
     displayScore()
     if gameState == 'start' then
         love.graphics.setFont(smallFont)
