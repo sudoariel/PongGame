1ad49f29b42b120544fe29ed8e55a2da17a5a345
Ariel Andrade <lima.ariel97@gmail.com>
2021-12-24 18:47:18 -0300

    updating score

--- main.lua
+++ main.lua
@@ -63,6 +63,20 @@ end
 
 function love.update(dt)
 
+    if ball.x < 0 then
+        servingPlayer = 1
+        player2Score = player2Score + 1
+        ball:reset()
+        gameState = 'serve'
+    end
+
+    if ball.x > VIRTUAL_WIDTH then
+        servingPlayer = 2
+        player1Score = player1Score + 1
+        ball:reset()
+        gameState = 'serve'
+    end
+
     if love.keyboard.isDown('w') then
         player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
